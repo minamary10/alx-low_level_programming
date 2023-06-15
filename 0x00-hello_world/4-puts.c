5a3127794a1681891d18eccf45a16de13ff1da47
Mina <monmina778@gmail.com>
2023-06-15 14:34:53 -0700

    finished

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,12 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints with put function
- * 
- * Return: Alawys 0 (Success)
+ *  Description: A C program that prints with put function.
+ *   Return: Alawys 0 (Success)
 */
 
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
