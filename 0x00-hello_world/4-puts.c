abedf3db9807541357c099f06ae33d1be843757a
Mina <monmina778@gmail.com>
2023-06-15 13:13:28 -0700

    finished

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+*include<atdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alwya O (Success)
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
